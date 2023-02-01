eeb30be1a93d36ec8db49d6a4cfb2f6c15e5fb16
keenroot <semerov111@yahoo.com>
2023-02-02 02:32:40 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const apiKey = 'f5ace00e37b44a0283d140316230102';
+
+//!!!поиск элементов на странице
+const main = document.querySelector('.main');
+const weatherContainer = document.querySelector('.weather__container');
+const form = document.querySelector('#form');
+const input = document.querySelector('#inputCity');
+
+//!!!вывод дефолтной карточки
+fillWeatherCard('Moscow');
+
+//!!!обработка события отправки формы
+form.onsubmit = function (event) {
+  event.preventDefault();
+  fillWeatherCard(input.value.trim());
+};
+
+async function fillWeatherCard(cityName) {
+  //!!!получение json из wether api
+  const jsonData = await getWeatherApi(cityName);
+  // console.log(jsonData);
+  //!!!удаление старой карточки
+  weatherContainer.remove();
+  //!!!добавление новой карточки с разметкро и данными из api
+  main.innerHTML = `<div class="weather__container">
+                      <span class="span__city">${jsonData.location.name}</span>
+                      <div class="weather__row">
+                        <span class="span__temperature">${jsonData.current.temp_c}<sup class="units">°с</sup></span>
+                        <img class="weather__img" src="./img/cloudy.svg" alt="weather" />
+                      </div>
+                      <span class="span__weather">
+                        ${jsonData.current.condition.text}
+                      </span>
+                    </div>`;
+}
+
+async function getWeatherApi(cityName) {
+  //запрос api
+  const respone = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}`
+  );
+  return await respone.json();
+}
