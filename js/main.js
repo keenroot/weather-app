725f28b891b4eb1c4c9c3000a9c2f305b1fd02eb
keenroot <38594245+keenroot@users.noreply.github.com>
2023-02-03 00:30:47 +0300

    Multilanguage

--- js/main.js
+++ js/main.js
@@ -6,40 +6,80 @@ const weatherContainer = document.querySelector('.weather__container');
 const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
 
-let cityName = 'Moscow';
-//!!!вывод дефолтной карточки
+let cityName = 'Москва';
+
+//!!! вывод дефолтной карточки
 fillWeatherCard();
 
-//!!!обработка события отправки формы
+//!!! обработка события отправки формы
 form.onsubmit = function (event) {
+  //!!! отключение релоада страницы
   event.preventDefault();
   cityName = input.value.trim();
   fillWeatherCard();
 };
 
 async function fillWeatherCard() {
-  //!!!получение json из wether api
-  const jsonData = await getWeatherApi();
-  // console.log(jsonData);
-  //!!!удаление старой карточки
-  weatherContainer.remove();
-  //!!!добавление новой карточки с разметкро и данными из api
-  main.innerHTML = `<div class="weather__container">
-                      <span class="span__city">${jsonData.location.name}</span>
-                      <div class="weather__row">
-                        <span class="span__temperature">${jsonData.current.temp_c}<sup class="units">°с</sup></span>
-                        <img class="weather__img" src="./img/cloudy.svg" alt="weather" />
-                      </div>
-                      <span class="span__weather">
-                        ${jsonData.current.condition.text}
-                      </span>
-                    </div>`;
+  //!!! получение json из wether api
+  const jsonWeatherData = await getWeatherApi();
+  //!!! получение json словаря с кондициями погоды
+  const jsonConditionsDictonary = await getConditionsDictonary();
+  //!!! поиск кода в словаре
+  const condition = jsonConditionsDictonary.filter(
+    (obj) => obj.code === jsonWeatherData.current.condition.code
+  );
+
+  /* ~~~плохой код~~~ используй тернарный оператор
+  const weatherObject = {
+    locationName: jsonWeatherData.location.name,
+    currentTemp_c: jsonWeatherData.current.temp_c,
+  };
+  if (jsonWeatherData.current.is_day) {
+    weatherObject.currentConditionText = condition[0].languages[23].day_text;
+  } else {
+    weatherObject.currentConditionText = condition[0].languages[23].night_text;
+  } ~~~плохой код~~~ */
+
+  //!!! объект с данными для вывода
+  const weatherObject = {
+    locationName: jsonWeatherData.location.name,
+    currentTemp_c: jsonWeatherData.current.temp_c,
+    //!!! получение дневной ночной кондиции погоды тернарным оператором из отфильтрованного словаря
+    currentConditionText: jsonWeatherData.current.is_day
+      ? condition[0].languages[23].day_text
+      : condition[0].languages[23].night_text,
+  };
+  //!!! вызов функции вывода
+  drawWeatherCard(weatherObject);
 }
 
 async function getWeatherApi() {
-  //запрос api
+  //!!! запрос api
   const respone = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}`
   );
   return await respone.json();
+}
+
+async function getConditionsDictonary() {
+  const respone = await fetch(
+    'https://www.weatherapi.com/docs/conditions.json'
+  );
+  return await respone.json();
+}
+
+function drawWeatherCard(weatherObject) {
+  //!!! удаление старой карточки
+  weatherContainer.remove();
+  //!!! добавление новой карточки с разметкро и данными из api
+  main.innerHTML = `<div class="weather__container">
+                        <span class="span__city">${weatherObject.locationName}</span>
+                        <div class="weather__row">
+                          <span class="span__temperature">${weatherObject.currentTemp_c}<sup class="units">°с</sup></span>
+                          <img class="weather__img" src="./img/cloudy.svg" alt="weather" />
+                        </div>
+                        <span class="span__weather">
+                          ${weatherObject.currentConditionText}
+                        </span>
+                      </div>`;
 }
